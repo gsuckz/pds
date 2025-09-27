b27235bb1ed17923a222ebc435135f883dea5d15
Jesus Avila <jesus.avila.05@gnail.com>
2025-09-27 17:45:55 -0300

    Corregido Nombre archivos en MostrarEspectro

--- MostrarEspectro.m
+++ MostrarEspectro.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
 
 % Archivos de entrada (uno por cada frecuencia de muestreo)
-archivos16khz = {'canal1_16_khz.wav', 'canal2_16_khz.wav', 'canal3_16_khz.wav'}; %Incluyo arhivos nombrados por su fsampleo
+archivos16khz = {'canal1_16khz.wav', 'canal2_16khz.wav', 'canal3_16khz.wav'}; %Incluyo arhivos nombrados por su fsampleo
 archivos24khz = {'canal1_24_khz.wav', 'canal2_24_khz.wav', 'canal3_24_khz.wav'}; %Incluyo arhivos nombrados por su fsampleo
 archivos32khz = {'canal1_32_khz.wav', 'canal2_32_khz.wav', 'canal3_32_khz.wav'}; %Incluyo arhivos nombrados por su fsampleo
 archivos = {archivos16khz,archivos24khz,archivos32khz};
